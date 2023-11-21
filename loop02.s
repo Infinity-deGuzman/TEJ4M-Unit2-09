6f24fc7ff2fabc24103bcdb7de95032386649056
infinity <infinity.deguzman@mths.ca>
2023-11-21 13:27:33 -0500

    figure out question 2 - loop02.s

---
+++ loop02.s
@@ -0,0 +1,16 @@
+/* -- loop02.s */
+.text
+.global main
+main:
+	mov r1, #0	@ r1 <- 0
+	mov r2, #1	@ r2 <- 1
+end:
+	mov r0, r1	@ r0 <- r1
+	bx	lr
+loop:
+	cmp r2, #100	@ compare r2 and 100
+	bgt end		@ branch if r2 > 100 to end
+	add r1, r1, r2	@ r1 <- r1 +r2
+	add r2, r2, #1	@ r2 <- r2 + 1
+	b	loop
+
